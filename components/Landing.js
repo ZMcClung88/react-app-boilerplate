6efb72bae7dbebc14533809ff01f029df4e4792f
Zac McClung <zac.mcclung370@gmail.com>
2017-12-06 21:39:04 -0700

    fixing changes accidentally made

--- components/Landing.js
+++ components/Landing.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import Time from './Time'
-import Quotes from './Quotes'
-import Greeting from './Greeting'
 
 const Landing = () => {
   return (
     <div className='landing-main'>
       <h1>Hello World</h1>
       <p>Welcome to my world</p>
-      <Time />
-      <Greeting />
-      <Quotes />
     </div>
   )
 }
