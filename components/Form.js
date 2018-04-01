b2978be95a43163e63b902a1b5a8679579c28506
Zac McClung <zac.mcclung370@gmail.com>
2018-03-31 22:48:28 -0600

    saving changes

--- components/Form.js
+++ components/Form.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  constructor(props) {
+    super(props) {
+      this.state = {
+        file: '',
+        imagePreviewUrl: ''
+      };
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    console.log('handle uploading- ', this.state.file);
+  }
+
+  handleImageChange(event) {
+    event.preventDefault();
+
+    let reader = new FileReader();
+    let file = event.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDaraURL(file)
+  }
   render() {
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if(imagePreviewUrl) {
+      $imagePreview = (<img src={imagePreviewUrl} />);
+    } else {
+      $imagePreview = (<div className=''>Please select an Image for preview</div>)
+    }
     return (
       <div>
+        <input placeholder="Image" type="file" />
         <input placeholder="Name" />
         <input placeholder="Occupation" />
-        <input placeholder="Image" />
         <input placeholder="linkedin" />
         <input placeholder="facebook" />
         <input placeholder="twitter" />
